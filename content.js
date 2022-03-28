5f0139c0e5b425a2c79404d2bc2e8dd7a1455c5b
samir <samir.dahal@crystalsolution.com.np>
2022-03-28 20:41:24 +0545

    add time ( clock )

--- content.js
+++ content.js
@@ -1,6 +1,7 @@
 let batteryInfo = null;
 let player = null;
 let currentUrl = null;
+let time = null;
 const playerState = {
   active: "active",
   inactive: "inactive",
@@ -19,8 +20,10 @@ const playerObserver = new MutationObserver(function (mutations) {
   mutations.forEach((mutation) => {
     if (mutation.target.classList.contains(playerState.active)) {
       updateBatteryInfo(batteryInfo);
+      updateTime(time);
     } else {
       hideBatteryInfo();
+      hideTime();
     }
   });
 });
@@ -51,9 +54,26 @@ function checkFullScreen(event) {
   player = document.fullscreenElement;
   if (player) {
     addBatteryInfo(player);
+    addTime(player);
   } else {
-    removeBatteryInfo(player);
+    removeBatteryInfo();
+    removeTime();
   }
+}
+function addTime(player) {
+  time = document.createElement("p");
+  time.style.margin = 0;
+  time.style.padding = 0;
+  time.style.position = "fixed";
+  time.style.top = 0;
+  time.style.left = 0;
+  time.style.fontSize = "16px";
+  time.style.color = "white";
+  player.appendChild(time);
+  updateTime(time);
+}
+function removeTime() {
+  time && time.remove();
 }
 function addBatteryInfo(player) {
   batteryInfo = document.createElement("p");
@@ -67,7 +87,7 @@ function addBatteryInfo(player) {
   player.appendChild(batteryInfo);
   updateBatteryInfo(batteryInfo);
 }
-function removeBatteryInfo(player) {
+function removeBatteryInfo() {
   batteryInfo && batteryInfo.remove();
 }
 function showBatteryInfo() {
@@ -80,6 +100,16 @@ function hideBatteryInfo() {
     batteryInfo.style.opacity = 0;
   }
 }
+function showTime() {
+  if (time) {
+    time.style.opacity = 1;
+  }
+}
+function hideTime() {
+  if (time) {
+    time.style.opacity = 0;
+  }
+}
 function updateBatteryInfo(batteryInfo) {
   if (batteryInfo) {
     navigator.getBattery().then((battery) => {
@@ -90,4 +120,20 @@ function updateBatteryInfo(batteryInfo) {
       showBatteryInfo();
     });
   }
+}
+function updateTime(time) {
+  if (time) {
+    time.textContent = formatAMPM(new Date());
+    showTime();
+  }
+}
+function formatAMPM(date) {
+  var hours = date.getHours();
+  var minutes = date.getMinutes();
+  var ampm = hours >= 12 ? "pm" : "am";
+  hours = hours % 12;
+  hours = hours ? hours : 12; // the hour '0' should be '12'
+  minutes = minutes < 10 ? "0" + minutes : minutes;
+  var strTime = hours + ":" + minutes + " " + ampm;
+  return strTime.toUpperCase();
 }
